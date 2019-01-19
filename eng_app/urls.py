fa16083eb6a907505fedaee69fe65a488b4923a7
Chiara Ripanti <chiararipanti@MacBook-Air-di-Chiara.local>
2019-01-19 18:57:11 +0000

    new file for acronym mode

--- eng_app/urls.py
+++ eng_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='my-home'),
     path('level/<int:level>', views.startGame, name='my-home'),
     path('acronyms', views.acronyms, name='acr'),
-    path('ac/type/<slug:type>', views.startAcronyms, name='acr'),
+    path('acronyms/type/<slug:type>', views.startAcronyms, name='acr'),
     path('update/<int:index>/<int:rightScore>/<int:wrongScore>/<int:isCorrect>', views.updateScore, name='update-score'),
 ]
